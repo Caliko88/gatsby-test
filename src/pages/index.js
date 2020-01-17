7f390fe45edd79d114c1b19a916567a5f61d6ed8
Joshua Antill <joshuaantill@pop-os.localdomain>
2020-01-17 13:34:46 -0600

    test.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new world.</p>
+    <p>Welcome to a new world.</p>
     <p>Hello World.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
